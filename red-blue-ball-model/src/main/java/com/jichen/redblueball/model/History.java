169e99d9fe9f9b10ce8d688a8915faccdfd6f626
陈洁 <chenyu530324@163.com>
2018-12-28 15:40:14 +0800

    Added get symmetrical number

--- red-blue-ball-model/src/main/java/com/jichen/redblueball/model/History.java
+++ red-blue-ball-model/src/main/java/com/jichen/redblueball/model/History.java
@@ -10,6 +10,7 @@ import java.util.TreeSet;
 
 public class History {
 
+    private static final int SYMMETRICAL = 34;
     private int number;
 
     private Date date;
@@ -34,56 +35,28 @@ public class History {
         return red1;
     }
 
-    public void setRed1(RedBall red1) {
-        this.red1 = red1;
-    }
-
     public RedBall getRed2() {
         return red2;
-    }
-
-    public void setRed2(RedBall red2) {
-        this.red2 = red2;
     }
 
     public RedBall getRed3() {
         return red3;
     }
 
-    public void setRed3(RedBall red3) {
-        this.red3 = red3;
-    }
-
     public RedBall getRed4() {
         return red4;
-    }
-
-    public void setRed4(RedBall red4) {
-        this.red4 = red4;
     }
 
     public RedBall getRed5() {
         return red5;
     }
 
-    public void setRed5(RedBall red5) {
-        this.red5 = red5;
-    }
-
     public RedBall getRed6() {
         return red6;
     }
 
-    public void setRed6(RedBall red6) {
-        this.red6 = red6;
-    }
-
     public BlueBall getBlueBall() {
         return blueBall;
-    }
-
-    public void setBlueBall(BlueBall blueBall) {
-        this.blueBall = blueBall;
     }
 
     public Set<RedBall> getRedBallSet() {
@@ -169,5 +142,10 @@ public class History {
             }
         }
         return differValues.size() - (redBalls.size() - 1);
+    }
+
+    public int getSymmetricalNumber(int index) {
+        List<RedBall> redBalls = new ArrayList<>(getRedBallSet());
+        return SYMMETRICAL - redBalls.get(index - 1).getValue();
     }
 }
