e36e609a6431721190769e51d0c58b8bd2924484
陈洁 <chenyu530324@163.com>
2018-12-28 16:26:21 +0800

    Refactory code

--- red-blue-ball-model/src/main/java/com/jichen/redblueball/model/History.java
+++ red-blue-ball-model/src/main/java/com/jichen/redblueball/model/History.java
@@ -192,10 +192,11 @@ public class History {
         if (n % 2 == 0) {
             return false;
         }
-        for (int i = 3; i * i <= n; i += 2)
+        for (int i = 3; i * i <= n; i += 2) {
             if (n % i == 0) {
                 return false;
             }
+        }
         return true;
     }
 
