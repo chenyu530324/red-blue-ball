2ce36f2e09cb4bc0aa03aafcb5e1c6b3c4b2350d
陈洁 <chenyu530324@163.com>
2018-12-29 17:09:56 +0800

    Added Annotation scanners

--- red-blue-ball-etl/src/main/java/com/jichen/redblueball/etl/Application.java
+++ red-blue-ball-etl/src/main/java/com/jichen/redblueball/etl/Application.java
@@ -1,5 +1,7 @@
 package com.jichen.redblueball.etl;
 
+import com.jichen.redblueball.etl.service.KillerEtl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.batch.JobExecutionExitCodeGenerator;
@@ -7,13 +9,20 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import static java.util.Collections.emptyList;
+
 @Configuration
 @ComponentScan(basePackages = {"com.jichen.redblueball"})
 @SpringBootApplication
 public class Application {
 
+    @Autowired
+    private KillerEtl killerEtl;
+
     public static void main(String[] args) {
         ConfigurableApplicationContext applicationContext = SpringApplication.run(Application.class, args);
+        KillerEtl killerEtl = (KillerEtl) applicationContext.getBean("killerEtl");
+        killerEtl.etl(emptyList());
         SpringApplication.exit(applicationContext, new JobExecutionExitCodeGenerator());
     }
 
