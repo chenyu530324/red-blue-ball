3eff224bc5a0a093dec4ac009fa7087bcbd417be
陈洁 <chenyu530324@163.com>
2019-01-02 17:58:18 +0800

    Fix build error

--- red-blue-ball-etl/src/main/java/com/jichen/redblueball/etl/service/KillerEtl.java
+++ red-blue-ball-etl/src/main/java/com/jichen/redblueball/etl/service/KillerEtl.java
@@ -1,14 +1,8 @@
 package com.jichen.redblueball.etl.service;
 
-<<<<<<< HEAD
 import com.jichen.redblueball.etl.configuration.ClassScannerService;
 import com.jichen.redblueball.model.History;
 import com.jichen.redblueball.model.annotations.Killer;
-=======
-import com.jichen.redblueball.model.History;
-import com.jichen.redblueball.model.annotations.Killer;
-import com.jichen.redblueball.model.annotations.Predictor;
->>>>>>> 2ce36f2e09cb4bc0aa03aafcb5e1c6b3c4b2350d
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -23,14 +17,7 @@ public class KillerEtl implements HistoryEtlService {
     @Override
     public boolean etl(List<History> historyList) {
         List<Killer> killerList = classScannerService.scanKillers();
-<<<<<<< HEAD
         return true;
-=======
-        List<Predictor> predictList = classScannerService.scanPredictors();
-        killerList.forEach(killer -> System.out.println(killer.name()));
-        predictList.forEach(predictor -> System.out.println(predictor.name()));
-        return false;
->>>>>>> 2ce36f2e09cb4bc0aa03aafcb5e1c6b3c4b2350d
     }
 
 
