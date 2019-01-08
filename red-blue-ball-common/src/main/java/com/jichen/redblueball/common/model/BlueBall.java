a193c69e9a9987fc8de7eea47c5c17a95d357874
陈洁 <chenyu530324@163.com>
2019-01-08 21:26:46 +0800

    Refactor code

--- red-blue-ball-common/src/main/java/com/jichen/redblueball/common/model/BlueBall.java
+++ red-blue-ball-common/src/main/java/com/jichen/redblueball/common/model/BlueBall.java
@@ -1,9 +1,9 @@
 package com.jichen.redblueball.common.model;
 
-import com.jichen.redblueball.common.BallType;
+import static com.jichen.redblueball.common.BallType.BLUE;
 
 public class BlueBall extends Ball {
-    public BlueBall(int value, BallType type) {
-        super(value, type);
+    public BlueBall(int value) {
+        super(value, BLUE);
     }
 }
--- red-blue-ball-common/src/main/java/com/jichen/redblueball/common/model/RedBall.java
+++ red-blue-ball-common/src/main/java/com/jichen/redblueball/common/model/RedBall.java
@@ -1,9 +1,9 @@
 package com.jichen.redblueball.common.model;
 
-import com.jichen.redblueball.common.BallType;
+import static com.jichen.redblueball.common.BallType.RED;
 
 public class RedBall extends Ball {
-    public RedBall(int value, BallType type) {
-        super(value, type);
+    public RedBall(int value) {
+        super(value, RED);
     }
 }
