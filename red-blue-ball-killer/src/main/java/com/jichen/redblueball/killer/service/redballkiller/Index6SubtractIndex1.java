907e145382e0eaf4d58b26973db7320e313e2167
陈洁 <chenyu530324@163.com>
2019-02-26 15:07:10 +0800

    Finished index 6 minus index 1

---
+++ red-blue-ball-killer/src/main/java/com/jichen/redblueball/killer/service/redballkiller/Index6SubtractIndex1.java
@@ -0,0 +1,19 @@
+package com.jichen.redblueball.killer.service.redballkiller;
+
+import com.jichen.redblueball.common.annotations.Killer;
+import com.jichen.redblueball.common.model.History;
+import com.jichen.redblueball.killer.service.KillerService;
+
+import java.util.Set;
+
+import static com.jichen.redblueball.common.BallType.RED;
+import static java.util.Collections.singleton;
+
+@Killer(name = "index6SubtractIndex1", type = RED)
+public class Index6SubtractIndex1 implements KillerService {
+
+    public Set<Integer> kill(History history) {
+        int killNumber = history.getRed6() - history.getRed1();
+        return singleton(killNumber);
+    }
+}
---
+++ red-blue-ball-killer/src/test/java/com/jichen/redblueball/killer/service/redballkiller/Index6SubtractIndex1Test.java
@@ -0,0 +1,19 @@
+package com.jichen.redblueball.killer.service.redballkiller;
+
+import com.jichen.redblueball.common.model.History;
+import org.junit.Test;
+
+import static com.jichen.redblueball.common.HistoryBuilder.aHistory;
+import static java.util.Collections.singleton;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class Index6SubtractIndex1Test {
+
+    @Test
+    public void shouldReturnCorrectNumberWhenGivenAHistory() {
+        Index6SubtractIndex1 killer = new Index6SubtractIndex1();
+        History history = aHistory().withRedBalls(4, 9, 17, 21, 25, 31).withBlueBall(1).build();
+        assertThat(killer.kill(history), is(singleton(27)));
+    }
+}
